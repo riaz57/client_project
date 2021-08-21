9ceb07e7fa944f53d89c67c3f855335b5300c316
Md. Riaz <68507939+riaz57@users.noreply.github.com>
2021-08-21 22:56:10 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+$(function(){
+
+        // carousel slider
+	$('#monitorscreen').owlCarousel({
+        loop:true,
+        margin:0,
+        mouseDrag:false,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+        nav:true,
+        dots:false,
+        items:1,
+        mouseDrag:false,
+        touchDrag:true,
+        autoplaySpeed:1500,
+        navSpeed:1500,
+        navText: ['<i class="fas fa-angle-left"><i>','<i class="fas fa-angle-right"><i>'],
+        navElement: 'a'
+    });
+
+        // sticky on scroll
+    $(window).on('scroll', function(e) {
+        var height = $('#background').outerHeight(true)/2;
+        var scrollHeight = $(window).scrollTop();
+
+        if(scrollHeight > height) {
+            $('.helpline').addClass('sticky');
+        }
+        else {
+            $('.helpline').removeClass('sticky');
+        }
+    });
+
+});
